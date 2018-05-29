32556e6c6eff01cffff25a4e629cc07bb69784c0
tiago.kenji <tiago.kenji@gs.liferay.com>
2018-05-29 15:44:32 -0300

    Create soy portlet

---
+++ modules/my-soy-portlet-project/src/main/java/com/liferay/docs/soyportlet/constants/MySoyPortletPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.docs.soyportlet.constants;
+
+/**
+ * @author tiago
+ */
+public class MySoyPortletPortletKeys {
+
+	public static final String MySoyPortlet = "MySoyPortlet";
+
+}
